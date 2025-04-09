169384bb3783c86171f5b2492f5c499e16184380
Manuel Vargas <vagm090504herrna8@lake.edu.mx>
2025-04-09 11:19:19 -0600

    Primer comit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Pokedex-manuel</h1>
+      <h2>Welcome to the Pokedex</h2>
+      <p>Here you can find information about your favorite Pokemon</p>
+
+      <div>
+        <p>Developed by Manuel Vargas Garcia</p>
+        <p>2025</p>
+        <p>All rights reserved</p>
+      </div>
+    </>
+  )
+}
+
+export default App
