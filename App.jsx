9302bfc1955383c283fae2df4122490e4cfe253b
Manuel <vagm090504hnerrna8@lake.edu.mx>
2025-05-14 07:52:11 -0600

    Profe

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+
 function App() {
   const [pokemones, setPokemones] = useState([]);
+
   useEffect(() => {
     const fetchPokemones = async () => {
-      const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=10"
-      );
+      const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
       const data = await response.json();
       const { results } = data;
 
@@ -18,37 +18,41 @@ function App() {
             id: datos.id,
             nombre: datos.name,
             imagen: datos.sprites.front_default,
-            altura:datos.height,
-            peso:datos.weight
+            altura: datos.height,
+            peso: datos.weight
           };
         })
       );
+
       setPokemones(detallesPokemon);
     };
+
     fetchPokemones();
   }, []);
 
   return (
-    <>
-      <h1>Pokedex - Manu</h1>
+    <div className="app-container">
+      <h1>Pokedex - Manuel Vargas</h1>
       <h2>Welcome to the Pokedex</h2>
-      <p>Here you can find information about your favorite Pokemon.</p>
+      <p>Here you can find information about your favorite Pokemon. I hope you enjoy it.</p>
 
-      {pokemones.map((pokemon) => (
-        <div className="card" key={pokemon.id}>
-          <h1>{pokemon.nombre} #({pokemon.id}) </h1>
-          <img src={pokemon.imagen} alt="" />
-          <p>Altura : {pokemon.altura / 10} m</p>
-          <p>Peso: {pokemon.peso / 10} kg </p>
-        </div>
-      ))}
+      <div className="card-container">
+        {pokemones.map((pokemon) => (
+          <div className="card" key={pokemon.id}>
+            <img src={pokemon.imagen} alt={pokemon.nombre} />
+            <h3>{pokemon.nombre} #{pokemon.id}</h3>
+            <p>Altura: {pokemon.altura / 10} m</p>
+            <p>Peso: {pokemon.peso / 10} kg</p>
+          </div>
+        ))}
+      </div>
 
-      <div>
-        <p>Developed by Alberto Pimentel</p>
+      <footer>
+        <p>Developed by Manuel Vargas Garcia</p>
         <p>2025</p>
         <p>All rights reserved</p>
-      </div>
-    </>
+      </footer>
+    </div>
   );
 }
 
